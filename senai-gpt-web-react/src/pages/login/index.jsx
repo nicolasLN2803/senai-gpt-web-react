f8b57136cf667cf2a966fd67baf696067354e2b1
nicolasLN2803 <nicolaslameiras@gmail.com>
2025-04-17 21:17:01 -0300

    FINALIZACAO DA TELA DE LOGIN

--- senai-gpt-web-react/src/pages/login/index.jsx
+++ senai-gpt-web-react/src/pages/login/index.jsx
@@ -4,33 +4,72 @@ import { useState } from "react";
 
 function Login() {
 
-const [ email, setEmail] = useState("");
-const [ senha, setSenha] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
 
 
 
 
-const onLoginCLick = async () => {
+  const onLoginCLick = async () => {
 
-let response = await fetch ("https://senai-gpt-api.azurewebsites.net/login", {
-  
-  headers:{
-    "Content-Type": "application/json"
-  },
-  method: "POST",
-  body: JSON.stringify({
+    let response = await fetch("https://senai-gpt-api.azurewebsites.net/login", {
 
-    
-    email: email,
-    senha: senha
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "POST",
+      body: JSON.stringify({
 
-  })
-});
 
-console.log(response);
+        email: email,
+        password: password
 
-}
+      })
+    });
+
+    console.log(response);
+
+    if (response.ok == true) {
+
+      alert("Login realizado com sucesso!");
+        console.log(response) 
+
+          let json = await response.json();
+
+          let token = json.accessToken;
+
+          console.log("Token: " + token);
+
+          // LOCAL STORAGE
+          localStorage.setItem("mytoken", token);
+
+          // // COOKIES
+
+          // function setCookie(name, value, days) {
+          //   const date = new Date();
+          //   date.setTime(date.getTime() + (days*24*60*60*1000)); // dias para milissegundos
+          //   const expires = "expires=" + date.toUTCString();
+          //   document.cookie = `${name}= ${value}; ${expires}; path=/`;
+          // }
+
+          // setCookie("meuToken", token,7);
+
+          window.location.href = "/chat";
+
+    } else {
+
+      if (response.status == 401) {
+
+        alert("Credenciais Incorretas. Tente novamente.");
+      } else {
+
+        alert("Erro inesperado. Caso persista, contate os administradores.")
+      }
+
+    }
+
+  }
 
   return (
     <>
@@ -46,8 +85,8 @@ console.log(response);
             id="meutitulo"
             className="titulo"> Login </h1>
 
-          <input className="inpt" value={email} onChange={event => setEmail(event.target.value)}type="email" placeholder=" Insira o email" />
-          <input className="inpt" value={senha} onChange={event => setSenha(event.target.value)}type="senha" placeholder=" Insira a senha" />
+          <input className="inpt" value={email} onChange={event => setEmail(event.target.value)} type="email" placeholder=" Insira o email" />
+          <input className="inpt" value={password} onChange={event => setPassword(event.target.value)} type="password" placeholder=" Insira a senha" />
 
           <button className="btm" onClick={onLoginCLick}> Entrar </button>
         </div>
